be13382ef85c277bf1815a7f9757393cd6097006
marjoriecanillo <marjoriecanillo20@gmail.com>
2025-08-18 23:53:41 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,6 @@
+username = 'marjorie'
+password = '2025'
+
+print(username == 'marjorie' )
+print((username == 'marjorie' ) and (password == '2025' ))
+print((username == 'marjorie' ) or (password == '2025' ))
