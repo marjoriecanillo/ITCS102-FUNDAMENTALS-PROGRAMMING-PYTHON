a6b69079cfbe5579002b14bd3ebad7514434451f
marjoriecanillo <marjoriecanillo20@gmail.com>
2025-09-12 05:51:22 -0700

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,10 @@
+username = "marjorie"
+pwd = "123456"
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "marjorie"and password == "123456":
+	print("Access Granted")
+else:
+	print("Access Denied")
