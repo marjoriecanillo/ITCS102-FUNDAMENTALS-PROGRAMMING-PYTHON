4546bd83971278d52adf2affa05b3b8aa8861988
marjoriecanillo <marjoriecanillo20@gmail.com>
2025-09-23 20:46:56 -0700

    Add files via upload

---
+++ code_challenge10.py
@@ -0,0 +1,10 @@
+#print("\t\t *",end=" ")
+for y in range(1, 11, 1):
+	for x in range(10, y, -1):
+	  print(" ",end =' ')
+	for i in range(1, y, 1):
+	  print("*", end =' ')
+	for z in range(1, y, 1):
+	 print("*", end =' ')
+	print()
+	 
